f1903dc649885d8d04f6931f339429bda67a27ca
Marcelo Costa <>
2026-02-01 16:09:03 -0400

    Adding Tween to Main Menu

--- BaseScenes/main_menu_canvas.gd
+++ BaseScenes/main_menu_canvas.gd
@@ -9,8 +9,14 @@ func _ready():
 
 func _on_start_game_button_pressed():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	colorRect.visible = false #TODO: Add a cool tween instead
 	UI.visible = false
+	var tween = get_tree().create_tween().set_parallel(true)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_EXPO)
+	tween.tween_property(colorRect, "color", Color(colorRect.color.r, colorRect.color.g, colorRect.color.b, 0), 1.2)
+	await tween.finished
+	
+	colorRect.visible = false
 	Globals.enable_player_control()
 
 func _on_end_game_button_pressed():
